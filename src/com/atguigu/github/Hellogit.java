a188830e3bc2629f823fd0d09a5eafed71c1b758
jingercome <601376657@qq.com>
2016-08-01 22:31:57 +0800

    22
    22

--- src/com/atguigu/github/Hellogit.java
+++ src/com/atguigu/github/Hellogit.java
@@ -7,5 +7,6 @@ public class Hellogit {
 		System.out.println("hello git hub");
 		System.out.println("hello git hub");
 		System.out.println("11111111");	
+		System.out.println("github say hello");	
 	}
 }
