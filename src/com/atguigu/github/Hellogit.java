bdd9142b8b1a1cb37bdf1ce8c9a769fbdd51bf10
jingercome <601376657@qq.com>
2016-08-01 22:12:47 +0800

    第二次提交

--- src/com/atguigu/github/Hellogit.java
+++ src/com/atguigu/github/Hellogit.java
@@ -4,5 +4,7 @@ public class Hellogit {
 	
 	public static void main(String[] args) {
 		System.out.println("hello git hub");
+		System.out.println("hello git hub");
+		System.out.println("hello git hub");
 	}
 }
